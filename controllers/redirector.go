69f7f355c7808e9df7e5a3918323f9b22526b021
peter andrew <peter.andrew987@gmail.com>
2024-07-16 11:13:58 +0700

    update redirect function to accomodate better redirection

--- controllers/redirector.go
+++ controllers/redirector.go
@@ -1,9 +1,11 @@
 package controllers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"peterchu999/url-shorterner/services"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,8 +16,13 @@ func RedirectShort(c *gin.Context) {
 	shortUrl := c.Param("short")
 	longUrl, err := services.GetRedirectUrl(shortUrl)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		log.Println(longUrl, err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+	if !(strings.Contains(longUrl, "http:") || strings.Contains(longUrl, "https:")) {
+		longUrl = fmt.Sprintf("https://%s", longUrl)
+	}
+
 	c.Redirect(http.StatusPermanentRedirect, longUrl)
 }
